fadedd999240bac8af2b26c120e1c58203038d13
Jonathan Casillas <react.jonathan.casillas@gmail.com>
2021-07-18 01:01:40 -0500

    Se agrego el archivo FadeInImage.tsx y useAnimation.tsx

--- src/Hooks/usePokemonPaginated.tsx
+++ src/Hooks/usePokemonPaginated.tsx
@@ -1,25 +1,47 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { pokemonApi } from '../Api/pokemonApi';
+import { PokemonPaginatedResponse, SimplePokemon, Result } from '../interfaces/pokeInterfaces';
 
 
 const usePokemonPaginated = () => {
     
-    
+    const [isLoading, setIsLoading] = useState(true);
+    const [simplePokemonList, setSimplePokemonList] = useState<SimplePokemon[]>([]);
     const nextPageURL = useRef('https://pokeapi.co/api/v2/pokemon?limit=40')
  
     const loadPokemons = async () => {
 
-        const resp = await pokemonApi.get(nextPageURL.current);
-        console.log(resp.data);
+        const resp = await pokemonApi.get<PokemonPaginatedResponse>(nextPageURL.current);
+        nextPageURL.current = resp.data.next;
+        mapPokemonList( resp.data.results );
+    }
+
+    const mapPokemonList =  ( pokemonList: Result[]  ) => {
+
+        const newPokemonList: SimplePokemon[] = pokemonList.map( ({name, url }) => {
+
+            const urlParts = url.split('/')
+            const id = urlParts[ urlParts.length - 2 ] ;
+            const picture = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`
+            
+            return {id, picture, name};
+
+        });
+
+        setSimplePokemonList( [...simplePokemonList, ...newPokemonList  ] );
+        setIsLoading(false);
 
     }
+
     
     useEffect(() => {
         loadPokemons();
     }, [])
 
     return {
-        
+        isLoading,
+        simplePokemonList,
+        loadPokemons,
     }
 }
 
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,15 +1,16 @@
 import React from 'react'
-import { View, Text, Image } from 'react-native'
+import { View, Text, Image,FlatList, ActivityIndicator } from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import Icon from 'react-native-vector-icons/Ionicons';
 import usePokemonPaginated from '../Hooks/usePokemonPaginated';
 import { styles } from '../Theme/appTheme';
+import { FadeInImage } from '../components/FadeInImage';
 
 const HomeScreen = () => {
 
     const { top } = useSafeAreaInsets();
 
-    usePokemonPaginated();
+    const { simplePokemonList, loadPokemons } = usePokemonPaginated();
 
 
     return (
@@ -17,11 +18,42 @@ const HomeScreen = () => {
 
             <Image source={require('../assets/pokebola.png')}
                    style={styles.pokebolaBG } />
-            <Text style={{
+
+            <FlatList 
+                data={simplePokemonList }
+                keyExtractor={(poke) => poke.id  }
+                showsVerticalScrollIndicator={false}
+                renderItem={ ({item, index }) =>(
+                    <FadeInImage
+                        uri={ item.picture }
+                        style={{
+                            width: 100,
+                            height: 100,
+                        }}
+                    
+                    />
+                ) }
+                
+                // infinite Scroll
+                onEndReached={loadPokemons }
+                onEndReachedThreshold={0.4 }
+
+                ListFooterComponent={ (
+                    <ActivityIndicator 
+                        style={{
+                            height: 100,
+                        }} 
+                        size={ 20 }
+                        color='indigo'
+                        />) }
+
+            />
+
+            {/* <Text style={{
                 ...styles.title,
                 ...styles.globalMargin,
                 top: top + 20, 
-                }} > Pokedex</Text>
+                }} > Pokedex</Text> */}
 
         </>
     )
