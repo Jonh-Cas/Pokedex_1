82f30acd0dbf5f8952ad8c0310f580ea58c06869
Jonathan Casillas <react.jonathan.casillas@gmail.com>
2021-07-19 22:36:16 -0500

    Se termino la panatalla de PokemosScreen.tsx

--- src/screens/PokemonScreen.tsx
+++ src/screens/PokemonScreen.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity, Image, ActivityIndicator } from 'react-native'
 import { StackScreenProps } from '@react-navigation/stack';
 import { RootStackParams } from '../Navigation/StackNavigation';
 import Icon from 'react-native-vector-icons/Ionicons';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { FadeInImage } from '../components/FadeInImage';
+import usePokemon from '../Hooks/usePokemon';
+import PokemonDetails from '../components/PokemonDetails';
 
 
 
@@ -14,12 +16,14 @@ interface Props extends StackScreenProps<RootStackParams, 'PokemonScreen'> { };
 const PokemonScreen = ({ navigation, route }: Props) => {
 
     const { simplePokemon, color } = route.params;
-    const { name, id, picture } = simplePokemon; 
+    const { name, id, picture } = simplePokemon;
     const { top } = useSafeAreaInsets();
+
+    const { isLoading, pokemon } = usePokemon(id);
 
     return (
 
-        <View>
+        <View style={{ flex: 1 }} >
             {/* HeaderContainer */}
             <View style={{
                 ...styles.headerContainer,
@@ -29,42 +33,55 @@ const PokemonScreen = ({ navigation, route }: Props) => {
 
                 <TouchableOpacity
                     activeOpacity={0.8}
-                    onPress={ () => navigation.pop() }
+                    onPress={() => navigation.pop()}
                     style={{
                         ...styles.backBoton,
                         top: top + 7,
                     }}
 
                 >
-                    <Icon 
-                    name='arrow-back-outline'
-                    size={35}
-                    color='white'
+                    <Icon
+                        name='arrow-back-outline'
+                        size={35}
+                        color='white'
                     />
                 </TouchableOpacity>
-                
+
                 <Text
                     style={{
                         ...styles.pokemonName,
-                        top: top + 40, 
+                        top: top + 40,
                     }}
-                > { name + '\n' } # { id } </Text>
+                > {name + '\n'} # {id} </Text>
 
-                <Image  
+                <Image
                     source={require('../assets/pokebola-blanca.png')}
                     style={{
                         ...styles.pokebola,
                     }}
                 />
 
-                <FadeInImage 
-                    uri={ picture }
+                <FadeInImage
+                    uri={picture}
                     style={styles.pokemonImage}
                 />
 
-
-
             </View>
+
+            {/* Detallles y loading */}
+
+            {(isLoading)
+
+                ? (<View style={styles.loadingIndicator} >
+                    <ActivityIndicator
+                        size={50}
+                        color={color}
+                    />
+                </View>)
+
+                : <PokemonDetails pokemon={ pokemon } />
+            }
+
         </View>
     )
 }
@@ -104,6 +121,15 @@ const styles = StyleSheet.create({
         height: 250,
         position: 'absolute',
         bottom: -15,
+    },
 
+    loadingIndicator: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
     }
+
 });
+
+
+
